095f5b22d6ba1f0b4b800ebbbbf6fa1567e37c24
somaya_abodabeel <92024063+somayaabodabeel@users.noreply.github.com>
2021-10-11 20:08:51 +0300

    Add files via upload

---
+++ Untitled8.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+for a in range(151):
+    print(a)
+
+
+# In[2]:
+
+
+
+for b in range(5, 1001, 5):
+    print(b)
+
+
+# In[14]:
+
+
+for c in range(1, 101):
+    if c % 5 == 0:
+        print("Coding")
+    elif c % 10 == 0:
+        print("Coding Dojo")
+    else:
+        print(c)
+
+
+
+
+
+
+# In[4]:
+
+
+
+
+sum = 0
+for d in range(500001):
+ if d % 2 != 0:
+    sum += d
+ print(sum)
+
+
+# In[6]:
+
+
+for x in range(2018, 0, -4):
+ if x > 0:
+  print(x)
+
+
+# In[20]:
+
+
+lowNum = 2
+highNum = 9
+mult = 3
+for f in range(lowNum, highNum + 1):
+ if f % mult == 0:
+  print(f)
+
+
+# In[ ]:
+
+
+
+
